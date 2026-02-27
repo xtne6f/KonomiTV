xtne6f/Konomi

Add: [Server] 通知領域にアイコンを表示できるようにする

server/KonomiTV-NoConsole.pyw
@@ -1,0 +1,14 @@
+
+from ctypes import windll, c_wchar_p
+from pathlib import Path
+
+SW_HIDE = 0
+
+windll.shell32.ShellExecuteW(
+    0,
+    c_wchar_p('open'),
+    c_wchar_p('.\\thirdparty\\Python\\python.exe'),
+    c_wchar_p('-m pipenv run python -X utf8 KonomiTV.py --notifyicon --delaysec 8'),
+    c_wchar_p(str(Path(__file__).resolve().parent)),
+    SW_HIDE
+)

server/prepare-and-start.ps1
@@ -165,4 +165,4 @@ $Env:PIPENV_VENV_IN_PROJECT = "true"
 
 .\thirdparty\Python\python.exe -m pipenv sync --python=$(Convert-Path .\thirdparty\Python\python.exe)
 .\thirdparty\Python\python.exe -m pipenv run aerich upgrade
-.\thirdparty\Python\python.exe -m pipenv run python -X utf8 KonomiTV.py
+.\thirdparty\Python\python.exe -m pipenv run python -X utf8 KonomiTV.py --notifyicon

xtne6f/KonomiTV

server/KonomiTV-NoConsole.pyw
@@ -1,0 +1,14 @@
+
+from ctypes import windll, c_wchar_p
+from pathlib import Path
+
+SW_HIDE = 0
+
+windll.shell32.ShellExecuteW(
+    0,
+    c_wchar_p('open'),
+    c_wchar_p('.\\thirdparty\\Python\\python.exe'),
+    c_wchar_p('-m poetry run python -X utf8 KonomiTV.py --notifyicon --delaysec 8'),
+    c_wchar_p(str(Path(__file__).resolve().parent)),
+    SW_HIDE
+)

server/prepare-and-start.ps1
@@ -190,4 +190,4 @@ pushd -LiteralPath $PSScriptRoot
 
 .\thirdparty\Python\python.exe -m poetry env use $(Convert-Path .\thirdparty\Python\python.exe)
 .\thirdparty\Python\python.exe -m poetry install --only main --no-root
-.\thirdparty\Python\python.exe -m poetry run python -X utf8 KonomiTV.py
+.\thirdparty\Python\python.exe -m poetry run python -X utf8 KonomiTV.py --notifyicon